29c4967f85dcdc793e7118980645e4a84d0fe138
Najla-88 <najlaaal018@gmail.com>
2023-08-19 10:53:49 +0300

    add validation
    add a new file validation.py to check validation for input data

--- add.py
+++ add.py
@@ -1,36 +1,43 @@
 from termcolor import colored
 from json import load, dump
+from validation import *
+
 
 def getDataFormJsonFile(_fileName):
     with open(_fileName, "r") as file_obj:
-        
         # load function read the JSON file content
         return load(file_obj)
 
+
 data = getDataFormJsonFile("db.json")
+
 
 def addNewData():
     # get the last id and increase it for the new ID
-    # TODO: add validations ...
-    id = data['contacts'][-1]["id"] + 1 
-    name = input("Enter Name: ")
-    phone = int(input("Enter Phone Number: "))
-    email = input("Enter Email: ")
+    id = data["contacts"][-1]["id"] + 1
+    validName = False
+    validEmail = False
+    validPhone = False
 
-    contact = {
-        "id": id,
-        "name": name,
-        "phone": phone,
-        "email": email
-    }
+    while not validName:
+        name = input("Enter Name: ")
+        validName = isValidName(name)
+
+    while not validPhone:
+        phone = input("Enter Phone Number: ")
+        validPhone = isValidPhone(phone)
+
+    while not validEmail:
+        email = "".join(input("Enter Email: ").split())
+        validEmail = isValidEmail(email)
+
+    contact = {"id": id, "name": name, "phone": phone, "email": email}
 
     # after get the JSON data add the new contact to them and reright the all file
     # with the updated data
-    data['contacts'].append(contact)
+    data["contacts"].append(contact)
     with open("db.json", "w") as file_obj:
-
         # dump function write in JSON file
         dump(data, file_obj)
-        
-        
+
         print(colored("Contact added successfully", color="green"))
--- update.py
+++ update.py
@@ -1,30 +1,32 @@
 from termcolor import colored
 from json import load, dump
 
+from validation import *
+
+
 def getDataFormJsonFile(_fileName):
     with open(_fileName, "r") as file_obj:
-
         # load function read the JSON file content
         return load(file_obj)
 
+
 data = getDataFormJsonFile("db.json")
 
+
 def updateDataFromId():
-    
     # bool var to check if the operation success or not
     found = False
     # while found != True:
-        
+
     # recive id of the deleted contact from user
     _id = int(input("Enter ID: "))
 
-    for contact in data['contacts']:
-
+    for contact in data["contacts"]:
         # check if there is a contact with this ID
-        if contact['id'] == _id:
+        if contact["id"] == _id:
             found = True
             break
-        
+
     else:
         print(colored("There is no contact with this ID", color="red"))
 
@@ -32,15 +34,35 @@ def updateDataFromId():
         print(colored("No change was happened!", color="red"))
 
     else:
+        validName = False
+        validEmail = False
+        validPhone = False
+
+        while not validName:
+            name = input("Enter Name: ")
+            validName = isValidName(name)
+
+        while not validPhone:
+            phone = input("Enter Phone Number: ")
+            validPhone = isValidPhone(phone)
+
+        while not validEmail:
+            email = "".join(input("Enter Email: ").split())
+            validEmail = isValidEmail(email)
+
         # change contact data
-        contact['name'] = input("Enter Name: ")
-        contact['phone'] = int(input("Enter Phone Number: "))
-        contact['email'] = input("Enter Email: ")
+        contact["name"] = name
+        contact["phone"] = phone
+        contact["email"] = email
 
         with open("db.json", "w") as file_obj:
-
             # dump function write in JSON file
             dump(data, file_obj)
 
             # TODO: color green
-            print(colored(f"Data of contact with ID {_id} updated successfully!", color="green"))
+            print(
+                colored(
+                    f"Data of contact with ID {_id} updated successfully!",
+                    color="green",
+                )
+            )
